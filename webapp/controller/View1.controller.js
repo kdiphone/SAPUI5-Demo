f0de10d80c4bbd6a4883361e2903b6987247045b
kdiphone <kdiphone24@gmail.com>
2021-04-26 03:34:47 +0000

    Promises Demo function

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -5,6 +5,71 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("AAAAAAAA.AAAAAAAA.controller.View1", {
+
+		fnSingleOpearation: function () {
+			var promise = new Promise(function(resolved, rejected){
+				var test = true;
+				if(test){
+					setTimeout(function(){
+						resolved("I have resolved");
+						console.log("1");
+					},200);
+					
+				}else{
+					rejected(Error(["Rejected"]));
+				}
+			});	
+			var promise11 = new Promise(function(resolved, rejected){
+				var test = true;
+				if(test){
+					setTimeout(function(){
+						resolved("I have resolved11");
+						console.log("2");
+					},100);
+				}else{
+					rejected(Error(["Rejected11"]));
+				}
+			});
+			var promise12 = new Promise(function(resolved, rejected){
+				var test = true;
+				if(test){
+					setTimeout(function(){
+						resolved("I have resolved12");
+						console.log("3");
+					}
+					);
+				}else{
+					rejected(Error(["Rejected12"]));
+				}
+				
+			});
+			/* Now you might need to perform some business operation when all of the three are resolved. In order to handle this 
+			asynchronous operation we use the promise.all function. */
+			Promise.race([promise,promise11,promise12]).then(function(param){
+					console.log(param);  //Traggers When promise completed
+				},function(param){
+					console.log("param_111111"); ////Traggers When promise Rejected
+				
+				});
+				
+				
+			/*var promise = new Promise(function(resolved, rejected){
+				var test = true;
+				if(test){
+					resolved("I have resolved");
+				}else{
+					rejected(Error(["Rejected"]));
+				}
+				promise.then(function(param){
+					console.log("param");  //Traggers When promise completed
+				}, function(param){
+					console.log("param_111111"); ////Traggers When promise Rejected
+				
+				});
+			});*/
+				
+		},
+
 		onInit: function () {
 			var oList = this.getView().byId("table1");
 			var oJsonModel = new sap.ui.model.json.JSONModel();
@@ -19,8 +84,8 @@ sap.ui.define([
 					oList.setModel(oJsonModel, "JSONMODEL");
 				}
 			});
-			
-			if(speechRecognition.checkAvailability){
+
+			if (speechRecognition.checkAvailability) {
 				console.log(speechRecognition.checkAvailability);
 			}
 
